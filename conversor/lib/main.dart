eff3fd273c0f856849af42509ac8d18ce4acc17b
boraaprender789 <95051027+boraaprender789@users.noreply.github.com>
2021-11-29 13:36:25 -0300

    Dando erro (Cannot run with sound null safety)
    Error: Cannot run with sound null safety, because the following dependencies
    don't support null safety:
    
     - package:http
     - package:http_parser
    
    For solutions, see https://dart.dev/go/unsound-null-safety

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
-void main(){
+const request = "https://api.hgbrasil.com/finance?format=json&key=098dd66a";
+
+void main() async{
+  http.Response response = await http.get(request);
+
+  print(response.body);
+
   runApp(MaterialApp(
     home: Container()
   ));
